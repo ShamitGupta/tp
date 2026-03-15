f284886744e73bb8ef69cd50e01a25e35088da73
alsonsim <alsonsimck@gmail.com>
2026-03-15 15:44:46 +0800

    Add JUnit tests for Duke

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -1,12 +1,31 @@
 package seedu.duke;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+public class DukeTest {
 
-class DukeTest {
     @Test
-    public void sampleTest() {
-        assertTrue(true);
+    public void logo_hasCorrectLineCount() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        assertEquals(5, logo.split("\n").length);
+    }
+
+    @Test
+    public void greeting_withValidName_returnsHelloMessage() {
+        String name = "Alice";
+        String expected = "Hello " + name;
+        assertEquals(expected, "Hello " + name);
+    }
+
+    @Test
+    public void greeting_withEmptyName_returnsHelloMessage() {
+        String name = "";
+        assertTrue(("Hello " + name).startsWith("Hello"));
     }
 }
